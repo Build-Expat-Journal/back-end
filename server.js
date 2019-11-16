71293ce15eb47743b1dfaadca1f9300fcef6651f
Lorenzo Simpson <lorenzosimp@gmail.com>
2019-11-16 12:15:51 -0500

    adds cors to server

--- server.js
+++ server.js
@@ -2,10 +2,11 @@ const express = require('express')
 const apiRouter = require('./routers/api-router')
 const server = express();
 const helmet = require('helmet')
-
+const cors = require('cors')
 
 server.use(express.json())
 server.use(helmet())
+server.use(cors())
 server.use('/api', apiRouter)
 
 
