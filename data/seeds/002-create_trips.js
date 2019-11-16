9efa6ab2321d3e64221b3bd34bbfa286ac8ae0f1
Lorenzo Simpson <lorenzosimp@gmail.com>
2019-11-16 12:02:13 -0500

    changes to del

--- data/seeds/002-create_trips.js
+++ data/seeds/002-create_trips.js
@@ -1,7 +1,7 @@
 
 exports.seed = function(knex) {
   // Deletes ALL existing entries
-  return knex('trips').truncate()
+  return knex('trips').del()
     .then(function () {
       // Inserts seed entries
       return knex('trips').insert([
