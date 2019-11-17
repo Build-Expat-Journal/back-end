c7dde841e14873c6365789d9af64cc812a30c98a
Lorenzo Simpson <lorenzosimp@gmail.com>
2019-11-17 15:12:23 -0500

    adds more error handling to get photo

--- routers/photos-router.js
+++ routers/photos-router.js
@@ -6,6 +6,7 @@ router.get('/:id', async(req, res) => {
     const {id} = req.params;
     let photo = await photosDb.findPhoto(id)
     if (photo) res.status(200).json(photo)
+    else if (photo === []) res.status(404).json({ error: 'That photo does not exist'})
     else res.status(500).json({ error: 'Could not find that photo'})
 })
 
