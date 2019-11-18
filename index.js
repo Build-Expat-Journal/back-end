488f018a6621b7588fa9f2254d1b17f51031902e
sandramkimball <53502963+sandramkimball@users.noreply.github.com>
2019-11-18 13:11:32 -0800

    adjusted index and env

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
+require('dotenv').config();
 const server = require('./api/server.js');
 
-const port = process.env.PORT || 5000;
-server.listen(port, () => console.log(`Running on Port ${port}`));
+
+const {PORT} = process.env || 5000;
+
+server.listen(PORT, () => console.log(`Running on Port ${PORT}`));
 // 'https://test-expat-db.herokuapp.com/'
