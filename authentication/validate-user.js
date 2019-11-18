1fa5d1aa67aa7eb5e566ae4dd1d1eb451bb492aa
Lorenzo Simpson <lorenzosimp@gmail.com>
2019-11-18 15:37:52 -0500

    attempts to fix register

--- authentication/validate-user.js
+++ authentication/validate-user.js
@@ -6,11 +6,11 @@ module.exports = validateUser;
 function validateUser(user) {
     let errors = []
 
-    if (!user.username) {
-        errors.push('Please include a username')
-    } else if (!user.password) {
-        errors.push('Please provide a password')
-    }
+    // if (!user.username) {
+    //     errors.push('Please include a username')
+    // } else if (!user.password) {
+    //     errors.push('Please provide a password')
+    // }
 
     return {
         isSuccessful: !errors.length,
