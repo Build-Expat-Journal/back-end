20ef0b6bcb952ba6edf89e6466faf494574d8448
Lorenzo Simpson <lorenzosimp@gmail.com>
2019-11-19 17:46:01 -0500

    last commit completely broke, rolling back

--- knexfile.js
+++ knexfile.js
@@ -46,11 +46,6 @@ module.exports = {
     seeds: {
       directory: './data/seeds'
     },
-    pool: {
-      afterCreate: (conn, done) => {
-        conn.run('PRAGMA foreign_keys = ON', done);
-      }
-    }
   },
 
 };
