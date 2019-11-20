4408cfc41857756b44e9bfe70647c9bc5e2dac16
sandramkimball <53502963+sandramkimball@users.noreply.github.com>
2019-11-20 11:41:07 -0800

    testing

--- auth/auth-router.spec.js
+++ auth/auth-router.spec.js
@@ -1,28 +1,36 @@
 const db = require('../database/dbConfig.js');
+const request = require('supertest');
 const router = require('./auth-router.js');
 const {add, findBy} = require('../users/users-model.js');
 
 describe('auth model', function() {
     describe('register()', function() {
 
+        beforeEach(async () => {await db('users').truncate();})
+
         test('should return error for empty registration', async function(){
-            await add({});
-            request(router).post('/register').then(res=> {
-                expect(res.status).toBe(!newUser)
+            await add({
+                username: '1', 
+                password: '', 
+                first_name: '',
+                last_name: '',
+                email: '2',
+            });
+            return request(router).post('/register').then(res=> {
+                expect(res.status).toBe(500)
             })
         });
 
         test('should register user', async function(){
             let newUser = await add({
-                username: 'Bo', 
+                username: 'Bo1', 
                 password: 'Bo', 
                 first_name: 'Bo',
                 last_name: 'Bo',
-                email: 'Bo',
-                country_id: 1,
+                email: 'Bo2',
             });
             return request(router).post('/register').then(res=> {
-                expect(res.status).toBe(newUser)
+                expect(res.status).toBe(201)
             })
         });
 
