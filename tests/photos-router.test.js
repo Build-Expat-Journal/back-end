f1671a5f6a932a3243bb709ae70df46211e1b52e
Lorenzo Simpson <lorenzosimp@gmail.com>
2019-11-20 12:35:16 -0500

    adds more photo testing

--- tests/photos-router.test.js
+++ tests/photos-router.test.js
@@ -4,7 +4,7 @@ const photosDb = require('../data/helpers/photos-helpers')
 
 describe('photos router', () => {
     it('should return a 201 created for a new photo', async() => {
-        let req = await request(server)
+        let res = await request(server)
         .post('/api/photos')
         .send({
             trip_id: 6,
@@ -13,8 +13,18 @@ describe('photos router', () => {
             img_url: "https://images.unsplash.com/photo-1516975698824-571e2c952dbd?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80"
         })
 
-        let resStatus = await req.status
+        let resStatus = await res.status
         expect(resStatus).toEqual(201)
         
+    })
+
+    it('should return a 200 deleted and return a deleted message', async() => {
+        let id = 18
+        let res = await request(server).del(`/api/photos/${id}`)
+        let resStatus = res.status
+       
+
+        expect(resStatus).toEqual(200)
+        expect(res.body.message).toMatch(/deleted/i)
     })
 })
