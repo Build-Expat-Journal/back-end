a7f07b6e5ecde0d6fca8264a9eb6b606cb20adff
sandramkimball <53502963+sandramkimball@users.noreply.github.com>
2019-11-20 09:30:20 -0800

    adding more tests

--- users/users-model.spec.js
+++ users/users-model.spec.js
@@ -1,7 +1,9 @@
 const db = require('../database/dbConfig.js');
-const {add, find, remove} = require('./users-model.js');
+const {add, findBy, remove} = require('./users-model.js');
 
 describe('user model', function() {
+
+//ADD
     describe('add()', function() {
 
         beforeEach(async () => {await db('users').truncate();})
@@ -19,6 +21,38 @@ describe('user model', function() {
             const users = await db('users');
             expect(users[0].name).toBe('Jimbo Slice');
         });
-
     });
+
+//FIND
+    describe('findBy()', function() {
+
+        beforeEach(async () => {await db('users').truncate();})
+
+        test('should find user by filter', async function(){
+            await add({name:'Dobby'});
+            await findBy('Dobby');
+
+            const user = await db('users');
+            expect(user).toHaveReturnedWith('Dobby');
+        });
+    });
+
+
+//REMOVE
+    describe('remove()', function() {
+
+        beforeEach(async () => {await db('users').truncate();})
+
+        test('should remove a user', async function(){
+            await add({name:'Dobby'});
+            await remove({name:'Dobby'});
+            await findBy({Dobby});
+
+            const users = await db('users');
+            expect(users).toHaveLength(0);
+            expect(users).toBe(!'Dobby');
+        });
+    });
+
+
 });
